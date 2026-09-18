e142c9dc29c6eec84ae28046df06bb67468f07b5
JAGADANANDA BISWAL <jagadanandabiswal1818@gmail.com>
2026-09-18 23:40:59 +0530

    Add files via upload

---
+++ fruit.java
@@ -0,0 +1,17 @@
+class Fruit {
+    protected String name;
+    protected String taste;
+    protected String size;
+
+    public Fruit(String name, String taste, String size) {
+        this.name = name;
+        this.taste = taste;
+        this.size = size;
+    }
+
+       public void eat() {
+        System.out.println("Fruit: " + name);
+        System.out.println("Taste: " + taste);
+        System.out.println("Size: " + size);
+    }
+}
